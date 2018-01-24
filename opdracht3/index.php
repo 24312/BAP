6d3024149ae87c576af746d2d6bb3ff79e47ee50
24312 <35424990+24312@users.noreply.github.com>
2018-01-24 12:01:31 +0100

    Add files via upload

---
+++ opdracht3/index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> Opdracht3 </title>
+</head>
+<body>
+<?php
+$voorbeeld1 = 'bob';
+$voorbeeld2 = 'jong';
+$voorbeeld3 = 'oud';
+$voorbeeld4 = 'bab';
+$voorbeeld5 = 8;
+
+echo 'ik ben ' . $voorbeeld1 . ' en ik ben nu ' . $voorbeeld5. '.';
+
+if ($voorbeeld5 >= 10) {
+
+    echo' ouder dan 10';
+    } else { echo ' jonger dan 10';
+}
+
+
+?>
+</body>
+</html>
---
+++ opdracht4/readform.php
@@ -0,0 +1,7 @@
+<?php
+
+$voornaam = $_POST['voornaam'];
+$achternaam = $_POST['achternaam'];
+$favoriete_eten = $_POST['favoriete_eten'];
+echo  'hallo ' . $voornaam ." ". $achternaam . ' jouw favoriete eten is:' . $favoriete_eten.'.';
+
